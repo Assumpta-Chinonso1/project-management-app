8a9127666dbb09471646e9a97ba134f96e044952
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-08-04 13:08:10 +0100

    Project Manager App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
+import NoProjectSelected from "./Component/NoProjectSelected";
+import ProjectSideBar from "./Component/ProjectSideBar";
+import NewProject from "./Component/NewProject";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStateAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+       
+      };
+    });
+  }
+
+
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStateAddProject={handleStateAddProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar onStateAddProject={handleStateAddProject} />
+      {content}
+    </main>
   );
 }
 
 export default App;
+
